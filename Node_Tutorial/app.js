cc467ee982b82e7dcb41c89c33881ea0e8f10df0
Quang-Dobe <tdquangcr@gmail.com>
2022-03-31 17:35:31 +0700

    Update

--- Node_Tutorial/app.js
+++ Node_Tutorial/app.js
@@ -1,4 +1,6 @@
 const http = require('http')
+const fs = require('fs')
+const path = require('path')
 
 const sever = http.createServer((req, res) => {
     if (req.url === '/') {
@@ -14,8 +16,33 @@ const sever = http.createServer((req, res) => {
         //     }
         // }
         res.write("Thank you for visiting!")
+        res.end()
         return;
     }
+    if (req.url === '/file') {
+        // We want to control the size of information we want to response. But we have some problem here
+        // If we use stream for reading a file, lines in callback function will be executed after return-line executed. So it has no meaning
+        /*
+        const stream = fs.createReadStream(path.join(__dirname, 'content', 'temp.txt'), { highWaterMark: 10, encoding: 'utf-8'})
+        // Success reading file
+        stream.on('data', chunk => {
+            res.end(`<p>${chunk}</p>`)
+        })
+        // Accure some error
+        stream.on('error', err => {
+            res.write(err.message)
+        })
+        */
+        
+        // So to solve this problem, we will use blocking code (using readFileSync()-function)
+        // But if using this way, we can't not control the size of information we want to response
+        var data = fs.readFileSync(path.join(__dirname, 'content', 'temp.txt'), 'utf-8')
+        res.write(`The information of that file is:\n${data}`)
+        res.end()
+        return;
+        // So, to solve this problem, I think we can use some trick that don't use return-line and allow the program executed to the end of this callback function
+    }
+    
     res.end(`
     <h1>Not found</h1>
     <p>Sorry we can't find this page</p>
@@ -25,3 +52,4 @@ const sever = http.createServer((req, res) => {
 sever.listen(3000, () => {
     console.log("Running at port 3000...")
 })
+
--- Node_Tutorial/modules.js
+++ Node_Tutorial/modules.js
@@ -49,8 +49,8 @@ console.log(path.join(__dirname, 'ExternalModule.js'))
 
 
 // fs module
-const fs = require('fs')
-const path = require('path')
+// const fs = require('fs')
+// const path = require('path')
 
 // Blocking: 
 /*
@@ -67,7 +67,6 @@ fs.writeFileSync(path.join(__dirname, 'content', 'temp.txt'),
     { flag:'a' }
 )
 */
-
 
 // None-blocking
 /*
@@ -93,4 +92,64 @@ fs.readFile(path.join(__dirname, 'content', 'temp.txt'), 'utf-8', (err, data) =>
     // console.log("Temp file:", tempFile)
 })
 console.log("---Task 1 done")
+*/
+
+// Using stream for controling reading a file
+/*
+// We can control reading file process
+// highWaterMark: We can control the size of file for reading (I can't use exact vocab in this)
+const stream = fs.createReadStream(path.join(__dirname, 'content', 'temp.txt'), { highWaterMark: 10, encoding:'utf-8' })
+
+stream.on('data', result => {
+    console.log(result)
+})
+stream.on('error', err => {
+    console.log(err)
+})
+*/
+
+
+// Wrapping function
+/*
+const fs = require('fs')
+const path = require('path')
+const getText = path => {
+    // Promise is just a callback function and it look like try catch, offload all in callback function
+    // and if there is no error, the resolve() will execute the code in callback and opposite
+    // We can see that Promise look like a wrapping function
+    return new Promise((resolve, rejects) => {
+        fs.readFile(path, 'utf-8', (err, data) => {
+            if (err) {
+                rejects(err)
+            }
+            else {
+                resolve(data)
+            }
+        })
+    })
+}
+getText(path.join(__dirname, 'content', 'temp.txt')).then(result => console.log(result)).catch(err => console.log(err))
+*/
+
+
+// Event
+/*
+const EventEmitter = require('events')
+// on       listen for an event
+// emtit    emit an event
+const customEventEmitter = new EventEmitter()
+
+// Create a listen-event for listening an event called "response"
+customEventEmitter.on('response', (name, age) => {
+    console.log('Data received: Name:', name, '- Age:', age)
+})
+// Create an emit-event called "response"
+customEventEmitter.emit('response', 'Cabe', 21)
+// Note: We must listen before amit and The listen-event just listen to the event with the name we provided
+// We can understand that firstly, we create a listen-event for listening the event with given name
+// Than, the listen-event will wait for emitting an event with the same name as the name that listen-event is given
+// If the emit-event is create, line in callback function of listen-event will be executed
+customEventEmitter.on('notListenForResponse', () => {
+    console.log('Logic action here')
+})
 */
